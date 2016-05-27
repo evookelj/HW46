93205ccb095d99cd8c8a07749ed1a10186643231
Wilson Berkow <wilsonberkow@gmail.com>
2016-05-26 23:01:03 -0400

    Fix insert() by maintaining sizeL and sizeR values

--- RunMed.java
+++ RunMed.java
@@ -13,8 +13,8 @@ public class RunMed {
     //instance vars
     private ALMaxHeap leftHeap;  //for lower range of dataset
     private ALMinHeap rightHeap; //for upper range of dataset
-    int sizeL;
-    int sizeR;
+    private int sizeL;
+    private int sizeR;
 
     /*****************************************************
      * default constructor  ---  inits empty heap
@@ -32,6 +32,10 @@ public class RunMed {
 	    if (sizeR > sizeL) { return rightHeap.peekMin(); }
 	    return leftHeap.peekMax();
 	} else {
+            if (isEmpty()) {
+                // No sensible numeric value can be reported
+                return Double.NaN;
+            }
 	    return (rightHeap.peekMin()+leftHeap.peekMax())/2.0;
 	}
     }//O(1)
@@ -42,16 +46,29 @@ public class RunMed {
      *                getMedian() can run in constant time
      *****************************************************/
     public void insert( int addVal ) {
+        if (leftHeap.isEmpty() || addVal <= leftHeap.peekMax()) {
+            leftHeap.add(addVal);
+            sizeL++;
+        } else {
+            rightHeap.add(addVal);
+            sizeR++;
+        }
 	if (sizeR>sizeL) {
-	    while (sizeR-sizeL != 1) { leftHeap.add(rightHeap.removeMin()); }
-	    leftHeap.add(addVal);
+	    while (sizeR-sizeL > 1) {
+                leftHeap.add(rightHeap.removeMin());
+                sizeR--;
+                sizeL++;
+            }
 	} else {
-	    while (sizeL-sizeR != 1) { rightHeap.add(leftHeap.removeMax()); }
-	    rightHeap.add(addVal);
+	    while (sizeL - sizeR > 1) {
+                rightHeap.add(leftHeap.removeMax());
+                sizeL--;
+                sizeR++;
+            }
 	}
 
     }//O(?)
-    
+
     /*****************************************************
      * boolean isEmpty()  ---  tells whether a median may be calculated
      * postcondition: dataset structure unchanged
@@ -73,8 +90,10 @@ public class RunMed {
 	System.out.println( med.getMedian() ); //3
         med.insert(7);
 	System.out.println( med.getMedian() ); //4
+        System.out.println("left: " + med.leftHeap + "\nright:" + med.rightHeap + "\n");
         med.insert(9);
 	System.out.println( med.getMedian() ); //5
+        System.out.println("left: " + med.leftHeap + "\nright:" + med.rightHeap + "\n");
 		/*~~~V~~~~~~~~~~~~move~me~down~~~~~~~~~~~~~V~~~
 	~~~~~|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|~~~*/
     }//end main()
